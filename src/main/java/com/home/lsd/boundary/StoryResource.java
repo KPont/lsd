26c859033a4fef7b543e4fa1de611518c6d3f880
Sindt <czindt@gmail.com>
2017-11-02 00:44:22 +0100

    rest api fix

--- src/main/java/com/home/lsd/boundary/StoryResource.java
+++ src/main/java/com/home/lsd/boundary/StoryResource.java
@@ -6,19 +6,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import com.home.lsd.control.StoryController;
+import javax.ws.rs.core.Response.Status;
 
 @Path("api")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class StoryResource {
 
-	private final StoryController controller = new StoryController();
+	// private final StoryController controller = new StoryController();
 
 	@GET
 	public Response getAllStories() {
-		return controller.getAllStories().build();
+		return Response.status(Status.OK).build();
 	}
 
 }
