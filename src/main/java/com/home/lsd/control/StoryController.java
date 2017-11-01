90a8fe07baf668fbbb80b2fc9152a7f260ca9b45
Sindt <czindt@gmail.com>
2017-11-01 10:55:20 +0100

    added logic to story controller

--- src/main/java/com/home/lsd/control/StoryController.java
+++ src/main/java/com/home/lsd/control/StoryController.java
@@ -12,16 +12,43 @@ import com.home.lsd.entity.SystemStatus;
 @Stateless
 public class StoryController {
 
-	Facade facade = new Facade();
+	private final Facade facade = new Facade();
 
 	public ResponseBuilder createPost(JsonObject input) {
-		int storyId = input.getInt("hanesst_id");
 		String storyTitle = input.getString("post_title");
-		String storyLink = input.getString("");
+		String storyLink = input.getString("post_url");
 		String storyType = input.getString("post_type");
-		String userName = input.getString("");
-		facade.addStory(storyId, storyTitle, storyLink, storyType, userName);
-		return Response.status(Status.OK).entity(input);
+		String userName = input.getString("username");
+		String comment = input.getString("post_text");
+
+		switch (input.getString("post_type")) {
+
+		case "story":
+			facade.addStory(0, storyTitle, storyLink, storyType, userName);
+			break;
+
+		case "comment":
+			int storyId = input.getInt("hanesst_id");
+			if (storyExists(input)) {
+
+				facade.addCommentToStory(storyId, userName, comment);
+				break;
+			}
+			return Response.status(Status.BAD_REQUEST).entity(input);
+
+		case "poll":
+			break;
+
+		case "pollopt":
+			break;
+		}
+
+		return Response.status(Status.CREATED).entity(input);
+	}
+
+	private boolean storyExists(JsonObject input) {
+		int id = input.getInt("hanesst_id");
+		return facade.getStory(id) != null ? true : false;
 	}
 
 	public ResponseBuilder getLatest() {
