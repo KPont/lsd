dde3c7bcb183f1efb1b6fc6c335b8089257987e9
Sindt <czindt@gmail.com>
2017-11-04 13:02:44 +0100

    host DO change

--- src/main/java/com/home/lsd/boundary/Facade.java
+++ src/main/java/com/home/lsd/boundary/Facade.java
@@ -23,7 +23,7 @@ import com.home.lsd.persistence.MySQL;
  */
 public class Facade implements IBackend {
 
-	MySQL ms = new MySQL("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1:3306/lsd", "root", "admin");
+	MySQL ms = new MySQL("com.mysql.jdbc.Driver", "jdbc:mysql://46.101.111.112:3306/lsd", "root", "admin");
 
 	public void testConnection() throws Exception {
 		ArrayList<User> result = ms.getUsers();
